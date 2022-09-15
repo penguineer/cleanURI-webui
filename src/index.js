b000de84a91a5432bcbd28457f94bc6407378105
David Kilias <dkdent@netz39.de>
2022-09-15 16:58:22 +0200

    feat: add fork me on github ribbon

--- src/index.js
+++ src/index.js
@@ -3,9 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GitHubForkRibbon from 'react-github-fork-ribbon';
+
+const GitHubForkRibbon = () => (
+  <GitHubForkRibbon href="https://github.com/penguineer/cleanURI-webui"
+                    target="_blank"
+                    position="right"
+                    color="green">
+    Fork me on GitHub
+  </GitHubForkRibbon>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <GitHubForkRibbon />,
   <React.StrictMode>
     <App />
   </React.StrictMode>
