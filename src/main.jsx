6556581f267bbbfdf39bfa208a87346bb659456c
Stefan Haun <mail@tuxathome.de>
2025-09-10 12:11:05 +0200

    Replace react component by native Github fork ribbon

--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './main.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import GitHubForkRibbon from 'react-github-fork-ribbon';
-
+import 'github-fork-ribbon-css/gh-fork-ribbon.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <GitHubForkRibbon href="https://github.com/penguineer/cleanURI"
-		      target="_blank"
-                      position="right"
-                      color="orange">
-      Fork me on GitHub
-    </GitHubForkRibbon>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <a className="github-fork-ribbon" href="https://github.com/penguineer/cleanURI" data-ribbon="Fork me on GitHub"
+           title="Fork me on GitHub" rel='noopener noreferrer' target="_blank" />
+        <App/>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
